62598485df7cc94fdda822d06af334dfc2b09785
amawy1 <odooamawy@gmail.com>
2024-09-03 17:54:51 +0300

    partner_ledger_filter_by_salesperson

--- partner_ledger_filter_by_salesperson/__manifest__.py
+++ partner_ledger_filter_by_salesperson/__manifest__.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 {
     'name': "partner_ledger_filter_by_salesperson",
+    'description': """
+        <h1>Partner Ledger Filter by Salesperson</h1>
+            <p>This module adds a filter by salesperson to the partner ledger report.</p>
+                <ul>
+                    <li>Show salesperson column in partner ledger report.</li>
+                    <li>Ability to filter by salesperson</li>
+                    <li>Ability to export report with salesperson</li>
+                    <li>Compatible with Odoo 15.0.</li>
+                    <li>Easy to use and integrates seamlessly with existing reports.</li>
+                </ul>
+        """,
+    'category': 'Accounting',
     'author': "Odoo Developer",
     'version': '15.0',
     'depends': ['base', 'account_accountant', 'account_reports'],
+    'license': 'LGPL-3',
     'data': [
         'views/partner_ledger_view.xml',
     ],
