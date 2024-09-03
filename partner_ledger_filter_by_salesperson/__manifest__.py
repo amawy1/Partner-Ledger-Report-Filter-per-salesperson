bbff5298caa5a3a4e54bd755c3f98afabda8fb8e
amawy1 <odooamawy@gmail.com>
2024-09-03 19:38:48 +0300

    add iet_partner_ledger_custom

--- partner_ledger_filter_by_salesperson/__manifest__.py
+++ partner_ledger_filter_by_salesperson/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "partner_ledger_filter_by_salesperson",
     'description': "This module adds a filter by salesperson to the partner ledger report. It includes features like filtering, exporting reports with salesperson data, and seamless integration with existing reports.",
-    'category': 'Accounting',
+    'category': 'Addon',
     'author': "Odoo Developers",
     'version': '15.0',
     'depends': ['base', 'account_accountant', 'account_reports'],
